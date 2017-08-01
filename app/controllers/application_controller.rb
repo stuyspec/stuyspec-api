dd13e7e8a7f9e28fcd989379ec49e22927240721
Nicholas Yang <ny585@nyu.edu>
2017-08-01 00:20:17 -0400

    trying again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::API
     # Check that the `Origin` field matches our front-end client host
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] =  'POST, GET, OPTIONS, DELETE'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
 end

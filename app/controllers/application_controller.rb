69bf013a00605106f76d2279ad797115770ba9bb
Nicholas Yang <ny585@nyu.edu>
2017-08-01 00:04:14 -0400

    quick fix for cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :cors_before_filter
+
+  def cors_before_filter
+    # Check that the `Origin` field matches our front-end client host
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
+
 end

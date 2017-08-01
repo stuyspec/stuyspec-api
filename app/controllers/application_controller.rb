279e9739752dfab72c23879c97f51cf3d011e7dd
Nicholas Yang <ny585@nyu.edu>
2017-08-01 00:10:31 -0400

    moar headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
   def cors_before_filter
     # Check that the `Origin` field matches our front-end client host
     headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] =  'POST, GET, OPTIONS, DELETE'
   end
 
 end

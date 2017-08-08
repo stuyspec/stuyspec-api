51274dfc60844f03d826c4fd9b70d3c893a686a7
Nicholas Yang <ny585@nyu.edu>
2017-08-07 23:57:17 -0400

    added first_name last_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804210950) do
+ActiveRecord::Schema.define(version: 20170808033957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,7 +88,7 @@ ActiveRecord::Schema.define(version: 20170804210950) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "name"
+    t.string "first_name"
     t.string "nickname"
     t.string "image"
     t.string "email"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20170804210950) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "thumbnail"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

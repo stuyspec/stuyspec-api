a194b1cec144b66310563d9785f06bb35582d09b
Nicholas Yang <ny585@nyu.edu>
2017-08-18 19:52:20 -0400

    merged

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,4 +1,13 @@
 class Medium < ApplicationRecord
   belongs_to :article
   belongs_to :user
+
+  has_attached_file :file,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  validates_attachment :file,
+                       content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
+
+
+
 end

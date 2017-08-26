d8b1d6d88c9bd0f5bad2f554c57f6147d7dfd5b2
jerry1ye10 <jerry1ye10@gmail.com>
2017-08-26 02:15:19 -0400

    Added slugs to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825202208) do
+ActiveRecord::Schema.define(version: 20170826061308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170825202208) do
     t.text "content"
     t.integer "volume"
     t.integer "issue"
-    t.boolean "is_published"
+    t.boolean "is_Published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "section_id"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 20170825202208) do
     t.string "thumbnail"
     t.string "last_name"
     t.integer "role_id"
+    t.string "slug"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

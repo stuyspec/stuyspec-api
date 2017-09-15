cf1e31281b1c910b770181b7f2e9725d9e75f8ea
Jason Kao <jasonkao85@gmail.com>
2017-09-14 23:28:23 -0400

    added outquotes to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904052134) do
+ActiveRecord::Schema.define(version: 20170915032724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170904052134) do
     t.bigint "section_id"
     t.integer "rank", default: 1
     t.string "summary"
+    t.string "outquotes", default: [], array: true
     t.index ["section_id"], name: "index_articles_on_section_id"
   end
 

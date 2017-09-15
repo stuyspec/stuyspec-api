659b0cbef2b5349e340f970fba7bb28beb27db5a
Jason Kao <jasonkao85@gmail.com>
2017-09-14 23:24:09 -0400

    added more test articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :username, :email, :description, :slug)
+      params.require(:user).permit(:first_name, :last_name, :email, :description, :slug)
     end
 end

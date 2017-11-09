e9e71f0a9c63aa39ac64ed445a3af84b68b528af
jerry1ye10 <jerry1ye10@gmail.com>
2017-11-08 23:07:14 -0500

    added back routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :authorships
       resources :comments
+      resources :media
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

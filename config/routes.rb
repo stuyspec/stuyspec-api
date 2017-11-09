8916eadbdd50a0a43447c936d4a51b54f39795bd
jerry1ye10 <jerry1ye10@gmail.com>
2017-11-08 23:04:30 -0500

    fixed media to not include attachments

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,7 @@ Rails.application.routes.draw do
   end
   resources :articles do
     resources :authorships
-    resources :media
-    resources :comments
+      resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

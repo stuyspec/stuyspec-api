3b147dc005277e17c6a8e8ced79b15a9c02c2291
Jason Kao <jasonkao85@gmail.com>
2017-11-17 02:01:37 -0500

    fixed amazon region locations; aws things

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,3 @@
 Aws::VERSION =  Gem.loaded_specs["aws-sdk"].version
 
-if Rails.env == "production" 
-   S3_CREDENTIALS = { :access_key_id => ENV['access_key_id'], :secret_access_key => ENV['secret_access_key'], :bucket => ENV['bucket']} 
- else 
-   S3_CREDENTIALS = Rails.root.join("config/aws.yml")
-end
+S3_CREDENTIALS = Rails.root.join("config/aws.yml")

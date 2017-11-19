66bc275c4928f5398634ffad9427089fbd3558ef
Nicholas Yang <ny585@nyu.edu>
2017-11-18 23:03:31 -0500

    cleaned up repo, added urls to media all request

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109210835) do
+ActiveRecord::Schema.define(version: 20171117192736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,4 +143,6 @@ ActiveRecord::Schema.define(version: 20171109210835) do
   end
 
   add_foreign_key "articles", "sections"
+  add_foreign_key "authorships", "articles"
+  add_foreign_key "authorships", "users"
 end

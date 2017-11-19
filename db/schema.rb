1d99576b6ef7e0f5e860ff56aee93bfe066ca105
Jason Kao <jasonkao85@gmail.com>
2017-11-19 01:06:01 -0500

    rename media type migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109210835) do
+ActiveRecord::Schema.define(version: 20171119060337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20171109210835) do
     t.string "title"
     t.text "caption"
     t.boolean "is_featured"
-    t.string "type"
+    t.string "media_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "attachment_file_name"

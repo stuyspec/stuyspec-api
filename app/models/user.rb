f812082a008ddeb83f04ea35105f651bd03cd92c
Nicholas Yang <ny585@nyu.edu>
2017-11-20 02:36:49 -0500

    disabled confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
   has_many :articles, through: :authorships
   has_many :comments

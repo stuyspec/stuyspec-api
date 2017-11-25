0866b64002ec14809b0fc979f37c448d78499429
Nicholas Yang <ny585@nyu.edu>
2017-11-24 23:54:35 -0500

    Revert "small newspapers"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :newspapers
   resources :outquotes
   resources :sections
   resources :articles
--- db/migrate/20171125030442_change_issue_config.rb
+++
@@ -1,5 +0,0 @@
-class ChangeIssueConfig < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :newspapers, :issue_config, :issuu_config
-  end
-end

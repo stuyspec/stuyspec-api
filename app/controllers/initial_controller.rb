02f2a21283d3e4860c88f28c3ff164174f94f920
Nicholas Yang <ny585@nyu.edu>
2017-11-26 21:52:55 -0500

    added the medium urls to init call

--- app/controllers/initial_controller.rb
+++ app/controllers/initial_controller.rb
@@ -1,10 +1,17 @@
 class InitialController < ApplicationController
   def index
+    media_with_urls = Medium.all.map do |medium|
+      medium.attributes.merge({
+                                attachment_url: medium.attachment_url,
+                                medium_attachment_url: medium.medium_attachment_url,
+                                thumb_attachment_url: medium.thumb_attachment_url
+                              })
+    end
     render json: {
       :articles => Article.all,
       :sections => Section.all,
       :comments => Comment.all,
-      :media => Medium.all,
+      :media => media_with_urls,
       :users => User.all,
       :roles => Role.all,
       :user_roles => UserRole.all,

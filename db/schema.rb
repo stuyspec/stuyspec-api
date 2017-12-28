7902381d1176fa479e7d7747397d19184f52e422
Nicholas Yang <ny585@nyu.edu>
2017-12-28 18:56:03 -0500

    switched from is_featured to featured_media

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171228191923) do
+ActiveRecord::Schema.define(version: 20171228234922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,6 @@ ActiveRecord::Schema.define(version: 20171228191923) do
     t.string "url"
     t.string "title"
     t.text "caption"
-    t.boolean "is_featured"
     t.string "media_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

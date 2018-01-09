4d87138ac32282e01c5cee68664bbe7b8bd5eebd
Jason Kao <jasonkao85@gmail.com>
2018-01-09 08:53:07 -0500

    get_featured_subsection resolver

---
+++ app/graphql/resolvers/get_featured_subsection.rb
@@ -0,0 +1,21 @@
+class Resolvers::GetFeaturedSubsection < GraphQL::Function
+
+  argument :section_id, !types.ID
+  # return type from the mutation
+  type Types::SectionType
+
+  # the mutation method
+   # _obj - is parent object, which in this case is nil
+  # _args - are the arguments passed
+  # _ctx - is the GraphQL context (which would be discussed later)
+  def call(_obj, args, _ctx)
+    section = Section.find(args['section_id'])
+    if section.slug == '10-31-terror-attack'
+      return Section.find(slug: 'creative-response')
+    elsif section.slug == 'ae'
+      return Section.find(slug: 'music')
+    end
+    subsections = Section.where(parent_id: section.id)
+    return subsections[0]
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -94,6 +94,8 @@ Types::QueryType = GraphQL::ObjectType.define do
   field :topLevelSections, !types[Types::SectionType] do
     resolve -> (obj, args, ctx) { Section.where(parent_id: nil) }
   end
+
+  field :featuredSubsection, function: Resolvers::GetFeaturedSubsection.new
   
   field :topRankedArticles, function: Resolvers::GetTopRankedArticles.new
 

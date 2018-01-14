0cb94b4f331e2992b4288116eca4fe7d4c81ca9b
Jason Kao <jasonkao85@gmail.com>
2018-01-14 10:31:41 -0500

    seeds to optimize client-app design

--- db/seeds.rb
+++ db/seeds.rb
@@ -422,6 +422,67 @@ unless ENV['minimal']
                                 'past StuyHacks, it is amazing to see all the '\
                                 'improvements done over the years to benefit '\
                                 'programmers,” senior Kevin Zhang said.</p>'
+                     },
+                     {
+                       title: section.name + ' Database Article With Index ' + index.to_s + ' Numero Cuatro',
+                      summary: 'Unfortunately, all good things must come to an end.'\
+                               'We came into Stuyvesant last September, saved from'\
+                               'the unstructured summer.',
+                      section_id: section.id,
+                      rank: 1,
+                      slug: section.slug + '-database-article-with-index-' + index.to_s + '-numero-cuatro',
+                      volume: 108,
+                      issue: 1,
+                      is_published: true,
+                      content: '<p>StuyHacks held its fourth hackathon, StuyHacks'\
+                                'IV, on Saturday, May 27, and Sunday, May 28. The'\
+                                ' event provided an opportunity for 175 high-school'\
+                                'students from the U.S. and Canada to learn about '\
+                                'and to immerse themselves in computer science.'\
+                                '</p><p>A hackathon is an event where programmers'\
+                                ' can collaborate on a range of computer science '\
+                                'projects. The hackathon organizers secured the '\
+                                'sponsorship of a number of technology companies,'\
+                                'including Facebook, MakeSchool, RedSeal, and '\
+                                'ThoughtWorks, to subsidize the event. The event'\
+                                'was held in the Midtown office of ThoughtWorks.'\
+                                '</p><p>StuyHacks worked together with the computer'\
+                                'science department and the Dojo, which is an '\
+                                'afterschool community for computer science '\
+                                'enthusiasts, to recruit mentors to help teach at'\
+                                'the hackathon. “Mentors [were] generally college'\
+                                'students who are majoring in Computer Science or '\
+                                'Computer Engineering. Some mentors [were] also '\
+                                'students from Stuyvesant who are currently taking'\
+                                'post-AP level courses,” senior and StuyHacks '\
+                                'logistics director Prancing Goose said.</p><p>'\
+                                'During the hackathon, attendees were organized '\
+                                'into groups of two to four for a team-building '\
+                                'session. These teams competed against one another'\
+                                'as they worked on a variety of programming '\
+                                'projects, from developing a mobile app to building '\
+                                'robot.</p><p>Alongside the competitions, multiple'\
+                                'workshops were held throughout the event to teach'\
+                                'attendees different computer science skills and '\
+                                'languages. For example, junior Shakil Rafi led the '\
+                                'Android Development workshop, which taught '\
+                                'participants how to code apps for the Android '\
+                                'operating system. </p><p>At the end of StuyHacks IV'\
+                                ', a panel of judges evaluated the group projects'\
+                                'and gave out multiple awards based off of them.'\
+                                'Seniors Nicholas Ng, Anya Keller, Mikhail Kotlik,'\
+                                'and Daniel Monteagudo came in third place for the'\
+                                'best overall project award for their work on Politalk,'\
+                                'a website inspired by the 2016 election that connects'\
+                                'people with opposing political ideologies through '\
+                                'a chat app.</p><p>For future hackathons, StuyHacks'\
+                                'organizers hope to increase funding to expand the'\
+                                'event by reserving a larger venue and adding more'\
+                                'workshops. Despite this, StuyHacks IV was well '\
+                                'received by many of its attendees. “Having been to'\
+                                'past StuyHacks, it is amazing to see all the '\
+                                'improvements done over the years to benefit '\
+                                'programmers,” senior Kevin Zhang said.</p>'
                      }
                    ]
                   )

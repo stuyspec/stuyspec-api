4f8c63d46373d85fef30fb83062dc940815be042
Jason Kao <jasonkao85@gmail.com>
2018-01-24 11:23:25 -0500

    add index to subscribers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124130832) do
+ActiveRecord::Schema.define(version: 20180124161100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 20180124130832) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_subscribers_on_email", unique: true
   end
 
   create_table "user_roles", force: :cascade do |t|

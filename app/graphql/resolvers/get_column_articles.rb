f1d0720007c9b27004f8c4db2a4829d221e7de9e
Nicholas Yang <ny585@nyu.edu>
2018-01-23 23:04:19 -0500

    whoops forgot offset

--- app/graphql/resolvers/get_column_articles.rb
+++ app/graphql/resolvers/get_column_articles.rb
@@ -11,11 +11,16 @@ class Resolvers::GetColumnArticles < GraphQL::Function
     main_left_column_articles = Article.order_by_rank.offset(6).first(2)
     outquotes_article = Article
                           .order_by_rank
+                          .offset(6)
                           .joins(:outquotes)
                           .where.not(id: main_left_column_articles)
                           .first
     left_column_articles = [*main_left_column_articles, outquotes_article]
-    right_column_articles = Article.order_by_rank.where.not(id: left_column_articles).first(2)
+    right_column_articles = Article
+                              .order_by_rank
+                              .offset(6)
+                              .where.not(id: left_column_articles)
+                              .first(2)
     return [*left_column_articles, *right_column_articles]
   end
 end

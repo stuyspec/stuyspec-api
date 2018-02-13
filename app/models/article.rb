8c0306a5d5b5c1222aa398bedf3b3bbe2087ea94
Jason Kao <jasonkao85@gmail.com>
2018-02-12 19:03:04 -0500

    added preview generation to Article.init

--- app/models/article.rb
+++ app/models/article.rb
@@ -15,18 +15,32 @@ class Article < ApplicationRecord
            through: :authorships,
            dependent: :destroy,
            source: :user
-
   has_many :media, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :outquotes, dependent: :destroy
 
   def init
     self.update(is_published: false)
+
+    if self.summary.to_s.empty?
+        preview = self.content.split(' ')[0, 25].join(' ') + '...'
+    else
+      words = self.summary.split(' ')
+      if words.length > 25
+        preview = words[0, 25].join(' ') + '...'
+      else
+        preview = self.summary
+      end
+    end
+
+    # Replacing </p><p> is separate from the regex sub so we don't get two
+    # spaces in a row.
+    self.update(preview, preview.gsub('</p><p>', ' ').gsub(/<\/?[^>]*>/, ' '))
   end
 
   def self.order_by_rank
     Article
-      .joins("LEFT JOIN sections ON articles.section_id = sections.id")
+      .joins(:section)
       .order("articles.rank + 3 * sections.rank + 12 * articles.issue + 192 * articles.volume DESC")
   end
 

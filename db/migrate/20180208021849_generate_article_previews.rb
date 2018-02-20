a2200701afe829aa03a9319d2b69dcc9864ba34d
Jason Kao <jasonkao85@gmail.com>
2018-02-19 19:41:17 -0500

    fixed article preview generation in migration

--- db/migrate/20180208021849_generate_article_previews.rb
+++ db/migrate/20180208021849_generate_article_previews.rb
@@ -1,7 +1,8 @@
 class GenerateArticlePreviews < ActiveRecord::Migration[5.1]
   def up
+    Article.reset_column_information
     Article.all.each do |article|
-      article.update(preview: article.generate_preview())
-    end      
+      article.update(preview: article.generate_preview)
+    end
   end
 end

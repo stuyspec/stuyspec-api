9522b8307adf712fe729aaf2d20fd1d261bbf881
Jason Kao <jasonkao85@gmail.com>
2018-02-19 20:15:22 -0500

    removed unnecessary field s on article

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -5,17 +5,15 @@ Types::ArticleType = GraphQL::ObjectType.define do
   field :title, !types.String
   field :slug, !types.String
   field :content, !types.String
-  field :created_at, types.String
   field :preview, types.String
-  field :summary, types.String
+  field :volume, !types.Int
+  field :issue, !types.Int
+  field :rank, types.Int
+  field :section, !Types::SectionType
   field :comments, types[!Types::CommentType]
   field :contributors, types[!Types::UserType]
   field :media, types[Types::MediumType]
   field :outquotes, types[Types::OutquoteType]
-  field :issue, !types.Int
-  field :rank, types.Int
-  field :volume, !types.Int
-  field :section, !Types::SectionType
 
   field :created_at, types.String do
     resolve -> (obj, args, ctx) {

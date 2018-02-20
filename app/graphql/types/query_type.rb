4cbdb5f435ff9176a3de56404853dd2ffe23b787
Jason Kao <jasonkao85@gmail.com>
2018-02-19 20:03:45 -0500

    removed unnecessary queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,23 +3,9 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  field :allArticles, !types[Types::ArticleType] do
-    # resolve would be called in order to fetch data for that field
-    resolve -> (obj, args, ctx) { Article.all }
-  end
-
   field :allSections, !types[Types::SectionType] do
     # resolve would be called in order to fetch data for that field
     resolve -> (obj, args, ctx) { Section.all }
-  end
-
-  field :allUsers, !types[Types::UserType] do
-    # resolve would be called in order to fetch data for that field
-    resolve -> (obj, args, ctx) { User.all }
-  end
-
-  field :allComments, !types[Types::CommentType] do
-    resolve -> (obj, args, ctx) { Comment.all }
   end
 
   field :articlesBySectionID do
@@ -86,11 +72,6 @@ Types::QueryType = GraphQL::ObjectType.define do
     argument :email, !types.String
     description "Find an user by email"
     resolve ->(obj, args, ctx) { User.find_by(email: args["email"])}
-  end
-
-  field :allMedia, !types[Types::MediumType] do
-    # resolve would be called in order to fetch data for that field
-    resolve -> (obj, args, ctx) { Medium.all }
   end
 
   field :latestArticles, function: Resolvers::GetLatestArticles.new

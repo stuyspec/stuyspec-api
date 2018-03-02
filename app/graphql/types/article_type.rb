eb7b08073bab0452a4b020c943b1f6ef564f1578
Jason Kao <jasonkao85@gmail.com>
2018-03-02 06:40:44 -0500

    reversed article media order; fixes #97

--- app/graphql/types/article_type.rb
+++ app/graphql/types/article_type.rb
@@ -12,8 +12,15 @@ Types::ArticleType = GraphQL::ObjectType.define do
   field :section, !Types::SectionType
   field :comments, types[!Types::CommentType]
   field :contributors, types[!Types::UserType]
-  field :media, types[Types::MediumType]
   field :outquotes, types[Types::OutquoteType]
+
+  # We want media shown in the order they were uploaded, which is by
+  # reverse id.
+  field :media, types[!Types::MediumType] do
+    resolve -> (obj, args, ctx) {
+      obj.media.reverse
+    }
+  end
 
   field :published_comments, types[!Types::CommentType] do
     resolve -> (obj, args, ctx) {
@@ -21,6 +28,8 @@ Types::ArticleType = GraphQL::ObjectType.define do
     }
   end
 
+  # created_at must be in this specific format for it to be readable, as a
+  # timestamp, across all browsers.
   field :created_at, types.String do
     resolve -> (obj, args, ctx) {
       obj.created_at.strftime('%Y/%m/%d %H:%M:%S %z')

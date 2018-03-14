23401d123ac8af3b007e1d4f66d9e0d2e95ade3c
Jason Kao <jasonkao85@gmail.com>
2018-03-14 08:52:02 -0400

    create medium

---
+++ app/graphql/resolvers/create_medium.rb
@@ -0,0 +1,31 @@
+class Resolvers::CreateMedium < Resolvers::MutationFunction
+  # arguments passed as "args"
+  argument :title, !types.String
+  argument :article_id, !types.Int
+  argument :profile_id, !types.Int
+  argument :caption, !types.String
+  argument :media_type, !types.String
+  argument :attachmentBase64, as: :attachment do
+    type !type.String
+  end
+
+  # return type from the mutation
+  type Types::MediumType
+
+  # the mutation method
+   # _obj - is parent object, which in this case is nil
+  # args - are the arguments passed
+  # _ctx - is the GraphQL context (which would be discussed later)
+  def call(_obj, args, ctx)
+    validate_admin(ctx)
+    @medium = Medium.new(
+      title: args["title"],
+      article_id: args["article_id"],
+      profile_id: args["profile_id"],
+      caption: args["caption"],
+      media_type: args["media_type"],
+    )
+    generate_new_header(ctx) if @medium.save
+    return @medium
+  end
+end

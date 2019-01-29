6590b24d572f1332b402932918c01cc82ce5297a
Darius Jankauskas <d5926j@gmail.com>
2019-01-28 21:27:47 -0500

    re added api welcome message

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
     end
 
     get '/init', to: 'initial#index'
-    #get '/', to: 'initial#welcome'
+    get '/', to: 'initial#welcome'
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 

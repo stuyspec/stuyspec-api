71f2c0237947858c8d7f482e9dba687617f76a55
Darius Jankauskas <d5926j@gmail.com>
2019-08-02 16:51:02 -0400

    Factored out authentication logic

---
+++ app/graphql/authentication.rb
@@ -0,0 +1,32 @@
+module Authentication
+    def self.admin_is_valid(ctx)
+        headers = Authentication::get_headers(ctx)
+        user = Authentication::get_user(headers)
+        
+        client_id = headers['client']
+        token = headers['access-token']
+    
+        return user && user.is_admin?(token, client_id)
+      end
+      
+      def self.generate_new_header(ctx)
+        # currently disable header change
+
+        # headers = Authentication::get_headers(ctx)
+        # user = Authentication::get_user(headers)
+    
+        # client_id = headers['client']
+        # new_auth_header = user.create_new_auth_token(client_id)
+        # response = ctx[:response]
+        # response.headers.merge!(new_auth_header)
+      end
+    
+      def self.get_headers(ctx)
+        return ctx[:request].headers
+      end
+      
+      def self.get_user(headers)
+        uid = headers["uid"]
+        return User.find_by(email: uid)
+      end
+    end
--- app/graphql/resolvers/mutation_function.rb
+++ app/graphql/resolvers/mutation_function.rb
@@ -1,32 +1,5 @@
 class Resolvers::MutationFunction < GraphQL::Function
   
-  def admin_is_valid(ctx)
-    set_headers(ctx)
-    set_user(ctx)
-    
-    client_id = @headers['client']
-    token = @headers['access-token']
+  #insert mutation specific logic here
 
-    return @user && @user.is_admin?(token, client_id)
-  end
-  
-  def generate_new_header(ctx)
-    set_headers(ctx)
-    set_user(ctx)
-
-    client_id = @headers['client']
-    new_auth_header = @user.create_new_auth_token(client_id)
-    response = ctx[:response]
-    response.headers.merge!(new_auth_header)
-  end
-
-  private
-  def set_headers(ctx)
-    @headers = ctx[:request].headers
-  end
-  
-  def set_user(ctx)
-    uid = @headers["uid"]
-    @user = User.find_by(email: uid)
-  end
 end

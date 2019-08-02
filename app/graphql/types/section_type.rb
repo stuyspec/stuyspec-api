a8139a312b3c02f0d4c1a50d118651115dece866
Darius Jankauskas <d5926j@gmail.com>
2019-08-02 16:50:35 -0400

    Added unpublished_articles field

--- app/graphql/types/section_type.rb
+++ app/graphql/types/section_type.rb
@@ -10,7 +10,19 @@ Types::SectionType = GraphQL::ObjectType.define do
   field :subsections, types[Types::SectionType]
   field :articles, types[!Types::ArticleType] do 
     resolve -> (obj, args, ctx) {
-      Resolvers::ArticleQueryFunction.select_published(obj.articles)
+      obj.articles.published
+    }
+  end
+
+  field :unpublished_articles, types[!Types::ArticleType] do 
+    resolve -> (obj, args, ctx) {
+      if !Authentication::admin_is_valid(ctx)
+        # Unpublished articles may only be viewed by authorized users.
+        return GraphQL::ExecutionError.new("Invalid user token. Please log in.")
+      end
+
+      Authentication::generate_new_header(ctx)
+      return obj.articles.unpublished
     }
   end
 

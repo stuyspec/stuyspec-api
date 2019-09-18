e013f5c1c046f307fd97705f5f33100a560cb6f4
Darius Jankauskas <d5926j@gmail.com>
2019-09-18 04:22:03 -0400

    Fix syntax error

--- app/graphql/resolvers/update_article.rb
+++ app/graphql/resolvers/update_article.rb
@@ -10,8 +10,8 @@ class Resolvers::UpdateArticle < Resolvers::MutationFunction
   argument :volume, types.Int
   argument :issue, types.Int
   argument :contributors, types[!types.Int]
-  argument :is_published, types.Boolean,
-  argument :media_ids: types[!types.Int]
+  argument :is_published, types.Boolean
+  argument :media_ids, types[!types.Int]
 
   # return type from the mutation
   type Types::ArticleType

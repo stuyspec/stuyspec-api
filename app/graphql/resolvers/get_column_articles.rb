e0633963eeae178df4fce3089f89854ba79c7ea9
Andrey Sokolov <andrey.sokolov123@gmail.com>
2021-04-01 00:31:09 -0400

    revertrevert

--- app/graphql/resolvers/get_column_articles.rb
+++ app/graphql/resolvers/get_column_articles.rb
@@ -10,7 +10,13 @@ class Resolvers::GetColumnArticles < Resolvers::ArticleQueryFunction
   def call(_obj, _args, _ctx)
     main_left_column_articles = Article.order_by_rank.published.offset(6).first(3)
     left_column_articles = [*main_left_column_articles]
-    right_column_articles = Article.order_by_rank..published.offset(6).where.not(id: left_column_articles).first(2)
+    right_column_articles = 
+                            Article
+                              .order_by_rank
+                              .published
+                              .offset(6)
+                              .where.not(id: left_column_articles)
+                              .first(2)
     return [*left_column_articles, *right_column_articles]
   end
 end

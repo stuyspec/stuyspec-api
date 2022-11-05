c06ed23a964fe15226e697ec3485b01d18061c84
David Chen <dchen30@stuy.edu>
2022-11-05 19:58:58 -0400

    add index to before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy, :index]
 
   # GET /users
   def index
